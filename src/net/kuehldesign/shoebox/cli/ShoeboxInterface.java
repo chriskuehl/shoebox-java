d5ebcc07cf783c852f914ce602479f07843d0b37
Chris Kuehl <chris@techxonline.net>
2011-07-17 18:33:28 -0400

    Adding logic for command-line parameters and for getting the CWD.

--- src/net/kuehldesign/shoebox/cli/ShoeboxInterface.java
+++ src/net/kuehldesign/shoebox/cli/ShoeboxInterface.java
@@ -1,8 +1,22 @@
 package net.kuehldesign.shoebox.cli;
 
-public class ShoeboxInterface {
+import java.io.File;
 
+public class ShoeboxInterface {
     public static void main(String[] args) {
+        String command = null;
         
+        try {
+            command = args[0];
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.err.println("You must specify a command. Try \"help\".");
+            System.exit(100);
+        }
+        
+        File workingDirectory = new File(".");
+        
+        if (command.equals("init")) {
+            System.out.println("current directory: " + workingDirectory.getAbsolutePath());
+        }
     }
 }
