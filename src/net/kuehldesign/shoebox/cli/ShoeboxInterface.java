1654ee43f6e3ab4973760d6ce2dcf34c634de06f
Chris Kuehl <chris@techxonline.net>
2011-07-17 18:44:29 -0400

    If a directory is given as a parameter, it is now created (or used if already existing).

--- src/net/kuehldesign/shoebox/cli/ShoeboxInterface.java
+++ src/net/kuehldesign/shoebox/cli/ShoeboxInterface.java
@@ -13,7 +13,27 @@ public class ShoeboxInterface {
             System.exit(100);
         }
         
-        File workingDirectory = new File(".");
+        File workingDirectory = new File("");
+        
+        // is there an extra parameter for directory?
+        if (args.length >= 2) {
+            String newDirectory = args[1];
+            
+            if (newDirectory.endsWith(File.separator)) {
+                newDirectory = newDirectory.substring(0, newDirectory.length() - 1);
+            }
+            
+            workingDirectory = new File(newDirectory);
+            
+            if (workingDirectory.exists() && ! workingDirectory.isDirectory()) {
+                System.err.println("The directory specified already exists as a file.");
+                System.exit(101);
+            }
+            
+            if (! workingDirectory.exists()) {
+                workingDirectory.mkdirs();
+            }
+        }
         
         if (command.equals("init")) {
             System.out.println("current directory: " + workingDirectory.getAbsolutePath());
