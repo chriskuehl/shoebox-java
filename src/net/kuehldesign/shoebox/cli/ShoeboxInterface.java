a598fe057c6e49b85c42aa16651ae03f9e71c468
Chris Kuehl <chris@techxonline.net>
2011-07-17 20:05:21 -0400

    Adding 'status' command.

--- src/net/kuehldesign/shoebox/cli/ShoeboxInterface.java
+++ src/net/kuehldesign/shoebox/cli/ShoeboxInterface.java
@@ -1,6 +1,7 @@
 package net.kuehldesign.shoebox.cli;
 
 import java.io.File;
+import java.sql.SQLException;
 import net.kuehldesign.shoebox.exception.InstanceAlreadyExistsHereException;
 import net.kuehldesign.shoebox.exception.UnableToInitializeInstanceHereException;
 import net.kuehldesign.shoebox.exception.UnableToLoadInstanceException;
@@ -39,17 +40,17 @@ public class ShoeboxInterface {
             }
         }
         
+        ShoeboxInstance instance = null;
+        
+        try {
+            instance = new ShoeboxInstance(workingDirectory);
+        } catch (UnableToLoadInstanceException ex) {
+            System.err.println("This Shoebox instance is malformed.");
+            System.exit(104);
+        }
+        
         // branch out for each possible command
         if (command.equals("init")) {
-            ShoeboxInstance instance = null;
-            
-            try {
-                instance = new ShoeboxInstance(workingDirectory);
-            } catch (UnableToLoadInstanceException ex) {
-                System.err.println("There is already a (possibly malformed) Shoebox instance here.");
-                System.exit(104);
-            }
-            
             try {
                 instance.initialize();
             } catch (InstanceAlreadyExistsHereException ex) {
@@ -58,6 +59,21 @@ public class ShoeboxInterface {
             } catch (UnableToInitializeInstanceHereException ex) {
                 System.err.println("Unable to initialize instance here.");
                 System.exit(103);
+            }
+        } else if (command.equals("status")) {
+            try {
+                if (instance.instanceExistsHere()) {
+                    System.out.println("No instance exists here.");
+                } else if (! instance.isConfigured()) {
+                    System.out.println("Instance exists but has not been configured.");
+                } else {
+                    System.out.println("Instance is configured.");
+                    // TODO: more instance information
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                System.err.println("Unable to determine status of instance.");
+                System.exit(105);
             }
         }
     }
